06fca7e291a3372add428ace5dd6c136b15f041e
Hsuanchu <hsuanchu@hsuanchu.localdomain>
2026-04-30 10:42:53 +0200

    idle_move draft v1

--- src/cococo/drafts.py
+++ src/cococo/drafts.py
@@ -221,3 +221,187 @@ def find_max_vdp_set(
                 raise RuntimeError(msg)
 
         return vdp_dict, terminal_pairs_remainder, factory_times_temp
+
+
+def idle_move_back(
+        self,
+        schedule,
+        danger_qubits,
+        available_gaps,
+        danger_qubits_temp,
+        available_gaps_temp,
+        layout,
+        layers,
+        reduce_time_stamp: bool,
+        jump_harvesting: bool,
+        best_schedule: None | list,
+    ):
+        """
+        Subroutine of `optimize_layers` to move back qubits in dangerous positions asap.
+        """
+        def gap_distance(g, danger_qubit, gap):
+            try:
+                return nx.shortest_path_length(g, danger_qubit, gap)
+            except nx.NetworkXNoPath:
+                return float("inf")
+            
+        def check_gap_flag(schedule, gap):
+            if schedule["steiner"] is not None:
+                    for steiner in schedule["steiner"].values():
+                        for node in steiner[0]:
+                            if node == gap:
+                                return True
+                        for node in steiner[1]:
+                            if node == gap:
+                                return True
+            for path in schedule["vdp_dict"].values():
+                for node in path:
+                    if node == gap:
+                        return True
+            return False
+        
+        # instead of adding another schedule_temp, take schedule[-1], adapt it and replace
+        schedule_temp = schedule[-1].copy()
+        flag_idle_move = False
+
+        # distinguish between the cases with jump_harvest = True and False. If true, you need to check more than schedule[-1] but also the future layers from "best_schedule" which was retrieved during SA.
+        # we want to avoid moves if the qubits are included in the k_lookahead layers since we want to guarantee that the routing computed for the metric in SA is the same as effectively used in k_lookahead layers to exploit the SA optimization fully without destroying stuff
+        if jump_harvesting and best_schedule is None:
+            raise ValueError(
+                "If `jump_harvest = True` you need to give a best_schedule as input for idle_move_back!"
+            )
+
+        #!Try to move re-allocated qubits back into the left gaps (does not need to be the original position, in case some other gap is closer)
+        #!TODO priority ordering of the danger_qubits (those which appear earlier in upcoming layers must be attempted to be moved back first)
+        danger_qubits_copy = danger_qubits.copy()
+        logical_pos_temp = list(layout.values())
+        next_layers_copy = layers.copy()
+        layout_rev = {j: i for i, j in layout.items()}
+        layout_mod = layout.copy()
+        # filter the danger_qubits to those which are idling right now? to avoid useless runs
+        flattened_vdp_dict_current = [
+            item for pair in schedule_temp["vdp_dict"].keys() for item in pair
+        ]
+        if (
+            jump_harvesting
+        ):  # include the (remaining) k_lookahead layers for the current jump because we do not want to alter the stuff from SA for multiple k_lookahead
+            for layer in best_schedule:
+                for key in layer.keys():
+                    flattened_vdp_dict_current.append(key[0])
+                    flattened_vdp_dict_current.append(key[1])
+
+        danger_qubits_idling = {
+            qubit: time
+            for (qubit, time) in danger_qubits_copy.items()
+            if qubit not in flattened_vdp_dict_current
+        }
+
+        danger_qubits_idling = {
+            qubit: time for (qubit, time) in danger_qubits_idling.items() if time <= 0
+        }
+        idle_move_labels = []
+        vdp_dict = schedule_temp["vdp_dict"]
+        for danger_qubit in danger_qubits_idling.keys():
+            #! TODO the gap is currently deleted, is the gap part of layout?
+            # go through gaps and take the one to which a path is available
+            path_idle = None
+            g_copy = self.g.copy()
+            #initial_nodes = set(g_copy.nodes())
+                
+            if schedule_temp["steiner"] is not None:
+                for steiner in schedule_temp["steiner"].values():
+                    for node in steiner[0]: 
+                        if node != danger_qubit:
+                            g_copy.remove_node(node)
+                    for node in steiner[1]:
+                        if node in g_copy.nodes() and node != danger_qubit:   # and node != gap: #at least one node in steiner[1] is already in steiner[0]
+                            g_copy.remove_node(node)
+            for path in schedule_temp["vdp_dict"].values():
+                for node in path :
+                    if node in g_copy.nodes() and node != danger_qubit:  # and node != gap:
+                        g_copy.remove_node(node)
+
+            for pos in layout_mod.values():
+                if pos in g_copy.nodes() and pos != danger_qubit:  #!just in case you find a bug, this was node != gap before, i dont know why this worked before i moved this into an own method
+                    g_copy.remove_node(pos)
+
+
+            candidate_gaps = [
+                gap for gap in available_gaps
+                if not check_gap_flag(schedule_temp, gap)
+            ]
+
+            path_idle = None
+            ordered_gaps = sorted(
+            candidate_gaps,
+            key = lambda gap:gap_distance(g_copy, danger_qubit, gap)
+            )
+            if ordered_gaps:
+                opt_gap = ordered_gaps[0]
+                dist = gap_distance(g_copy, danger_qubit, opt_gap)
+                if dist < float("inf"):
+                    path_idle = nx.dijkstra_path(g_copy, opt_gap, danger_qubit)
+                    # danger_qubits_copy.remove(danger_qubit)
+                    del danger_qubits_copy[danger_qubit]
+                    available_gaps.remove(opt_gap)
+                    label_idle = f"idle_{danger_qubit}_to_{opt_gap}"
+                    vdp_dict.update({label_idle: path_idle}) #! TODO now we have string keys, will be troubled if we call this function again because of line 1378
+                    idle_move_labels.append(label_idle)
+                    logical_pos_temp = self.replace_pos(
+                        logical_pos_temp, danger_qubit, opt_gap
+                    )
+                    for j, next_layer in enumerate(
+                        next_layers_copy
+                    ):  # update all future layers
+                        next_layers_copy[j] = self.replace_pos(
+                            next_layer, danger_qubit, opt_gap
+                        )
+                    label = layout_rev[danger_qubit]
+                    layout_mod[label] = opt_gap
+                    layout_rev = {
+                        j: i for i, j in layout_mod.items()
+                    }  #!update layout_rev
+                    flag_idle_move = True
+                # because if path found you do not want to find another path to the same danger qubit
+            elif path_idle == None:
+                logger.info(
+                    f"No idling path back could be found for danger qubit {danger_qubit}"
+                )
+           
+                
+
+        # reduce the time stamp of danger_qubits_copy by 1
+        if reduce_time_stamp:
+            danger_qubits_copy = {
+                qubit: time - 1 for (qubit, time) in danger_qubits_copy.items()
+            }
+
+        danger_qubits = danger_qubits_copy.copy()
+        # add those danger qubits and empty spots which where added in this layer
+        # danger_qubits += danger_qubits_temp #those temp danger qubits have k_lookahead as timelabel
+        danger_qubits |= danger_qubits_temp
+        available_gaps += available_gaps_temp
+        
+        # if an element appears both in danger_qubits and available_gaps, this hints to the case that a qubit in a danger position was moved again. hence delete the double elements
+        shared_elements = set(danger_qubits.keys()) & set(available_gaps)
+        danger_qubits = {
+            qubit: time
+            for (qubit, time) in danger_qubits.items()
+            if qubit not in shared_elements
+        }
+        available_gaps = [x for x in available_gaps if x not in shared_elements]
+
+        #!update terminal_pairs, logical_pos etc
+        schedule_temp["vdp_dict"] = vdp_dict
+        self.logical_pos = logical_pos_temp.copy()
+        self.logical_pos_temp = logical_pos_temp.copy()
+        schedule_temp["logical_pos"] = self.logical_pos.copy()
+        schedule_temp["idle_move_label"] = idle_move_labels.copy()
+        # should I update schedule_temp["layout"] here? 
+        layers = next_layers_copy.copy()
+        layout = layout_mod.copy()
+
+        if flag_idle_move:
+            schedule[-1] = schedule_temp.copy()
+
+        return schedule, danger_qubits, available_gaps, layout, layers
